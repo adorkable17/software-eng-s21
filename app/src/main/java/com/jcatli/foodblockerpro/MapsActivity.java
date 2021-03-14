e846d9735a351f95751d4b6c973615852148566a
Justine C <47696696+adorkable17@users.noreply.github.com>
2021-03-14 16:27:14 -0400

    Hello World for Android Studio demo removed

--- app/src/main/java/com/jcatli/foodblockerpro/MapsActivity.java
+++ app/src/main/java/com/jcatli/foodblockerpro/MapsActivity.java
@@ -43,9 +43,4 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-
-    public void helloWorld()
-    {
-        System.out.print("Hello World!");
-    }
 }
